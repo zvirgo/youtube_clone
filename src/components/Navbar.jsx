f2e339f26e037ce1d558a864c96f5ef5250100dc
zeinali <zeinali.b69@gmail.com>
2022-09-07 09:42:41 +0430

    create Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Stack } from '@mui/system'
+import { Link } from 'react-router-dom'
+import { logo } from '../utils/constants'
+import SearchBar from './SearchBar'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack direction="row" alignItems="center" p={2}
+      sx={{
+        position: 'sticky',
+        background: '#000',
+        top: 0,
+        justifyContent: "space-between"
+      }}>
+      <Link to="/"
+        style={{
+          dispaly: 'flex',
+          alignItem: 'center'
+        }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
+      <SearchBar />
+    </Stack>
+
   )
 }
 
