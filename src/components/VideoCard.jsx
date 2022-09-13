0b40ff539915331512f43d3e6628afa742b31933
zeinali <zeinali.b69@gmail.com>
2022-09-13 12:26:33 +0430

    fix buggs of showing image in CardMedia

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,12 +4,12 @@ import { Typography, Card, CardContent, CardMedia } from '@mui/material'
 import { CheckCircle } from '@mui/icons-material'
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from "../utils/constants";
 
-const VideoCard = ({ video: { id: videoId }, snippet }) => {
+const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
     return (
-        <Card sx={{ width: { md: '320px', xs: '100%' }, boxShadow: 'none', borderRadius: 0 }}>
-            <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <Card sx={{ width: { xs: '100%', sm: '358px', md: "320px", }, boxShadow: 'none', borderRadius: 0 }}>
+            <Link to={videoId ? `/video/${videoId}` : `/video/cV2gBU6hKfY`}>
                 <CardMedia
-                    image={snippet?.thumbnails?.hight?.url}
+                    image={snippet?.thumbnails?.high?.url}
                     alt={snippet?.title}
                     sx={{ width: 358, height: 180 }}
                 />
