ccf84a46a614c2e3eea7598b82b718a2e119a509
zeinali <zeinali.b69@gmail.com>
2022-09-14 09:54:20 +0430

    fix style of videoCard in responsive size

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -11,7 +11,7 @@ const VideoCard = ({ video: { id: { videoId }, snippet } }) => {
                 <CardMedia
                     image={snippet?.thumbnails?.high?.url}
                     alt={snippet?.title}
-                    sx={{ width: 358, height: 180 }}
+                    sx={{ width: {xs:'100%',sm:'358px',md:'320px'}, height: 180 }}
                 />
             </Link>
             <CardContent sx={{ backgroundColor: '#1e1e1e', height: '106px' }} >
